fe6dbaf9e933936a863be321fb8eea19bbd2defb
Aleix Daroca <daroca.zaale23@zaragoza.salesianos.edu>
2026-05-22 08:08:14 +0200

    feat: primer commit de sistema financiero

---
+++ src/main/java/com/daroca/sistema_financiero/SistemaFinancieroApplication.java
@@ -0,0 +1,13 @@
+package com.daroca.sistema_financiero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaFinancieroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaFinancieroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daroca/sistema_financiero/SistemaFinancieroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daroca.sistema_financiero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaFinancieroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
